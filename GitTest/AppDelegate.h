3c409bfec067cc7a4f2c99fb86d412c3690a2743
Pokatovich Nikita <pokatovich.n@apalon.com>
2015-06-10 16:07:59 +0300

    first commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Kit Rouch on 6/10/15.
+//  Copyright (c) 2015 Kit Rouch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ GitTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by Kit Rouch on 6/10/15.
+//  Copyright (c) 2015 Kit Rouch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
